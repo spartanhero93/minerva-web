c160f0225655147d6504e21e3a0d0b3df2a6901e
Brodey Newman <brodeynewman@gmail.com>
2018-04-24 21:37:22 -0400

    setting up webpack & whatnot

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js&/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+  }
+}
